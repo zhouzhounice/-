e1f6f0fd0eaa3672497ffba6f986483be153df32
zhouzhounice <894425013@qq.com>
2022-05-28 09:27:33 +0800

    vue相关

---
+++ hello-world/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" v-model="baba" />
+    <h1>{{baba}}</h1>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      baba:"66667",
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <!-- <input type="text" v-model="test" /> -->
+    <!-- <input type="text" :value="test" @input="event => test=event.target.value"/> -->
+    <input type="text" :value="value" @input="handleInput"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+    value:String
+  },
+  data() {
+    return {
+      test:666,
+    }
+  },
+  methods: {
+    handleInput(event){
+      // console.log('handleInput')
+      const value = event.target.value
+      this.$emit('input',value)
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
